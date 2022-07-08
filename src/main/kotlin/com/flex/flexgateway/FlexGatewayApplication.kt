83ab6eb30c64042aadbb199b39361060c25e1f56
lyutvs <lyutvs@gmail.com>
2022-07-08 17:02:54 +0900

    ⚙️ :: 프로젝트 생성

---
+++ src/main/kotlin/com/flex/flexgateway/FlexGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.flex.flexgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlexGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlexGatewayApplication>(*args)
+}
